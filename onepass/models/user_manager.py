ec1589b6429bea1d3f92f9917774892a5113ef2a
bienkos <sewerynbienko@gmail.com>
2021-02-27 01:29:33 +0100

    Add UserManager

---
+++ onepass/models/user_manager.py
@@ -0,0 +1,39 @@
+import os
+
+from dotenv import load_dotenv
+from bcrypt import hashpw, gensalt, checkpw
+
+
+load_dotenv()
+SALT_LENGTH = int(os.getenv("SALT_LENGTH"))
+
+
+class UserManager:
+    def __init__(self, db_manager, validation_manager):
+        self.db = db_manager
+        self.v = validation_manager
+
+    def register(self, name, password, password1, master, master1):
+        user = {'username': name, 'password': password,
+                'password1': password1, 'master': master, 'master1': master1}
+        if not self.v.is_user_valid(user):
+            return False
+        hashed = self.hash_pass(user['password'])
+        master_hashed = self.hash_pass(user['master'])
+        if self.db.register_user(user['username'], hashed, master_hashed):
+            return True
+        return False
+
+    def hash_pass(self, password, salt_length=SALT_LENGTH):
+        password, salt = password.encode(),  gensalt(salt_length)
+        return hashpw(password, salt)
+
+    def login(self, username, password):
+        if not self.v.is_username_login_valid(username) or not self.v.is_password_login_valid(password):
+            return False
+        if self.db.is_registred(username):
+            hashed = self.db.get_user_password(username)
+            if hashed:
+                if checkpw(password.encode(), hashed):
+                    return True
+        return False
--- run.py
+++ run.py
@@ -12,13 +12,13 @@ import uuid
 from onepass.db.db_manager import DbManager
 from onepass.models.aes import AESCipher
 from onepass.models.validation import Validator
+from onepass.models.user_manager import UserManager
 
 APP_NAME = "onepass"
 load_dotenv()
-DB_PATH = os.getenv("DATABASE")
-SALT_LENGTH = int(os.getenv("SALT_LENGTH"))
-db = DbManager(DB_PATH)
+db = DbManager(os.getenv("DATABASE"))
 v = Validator()
+um = UserManager(db, v)
 
 # Configurations
 HA = "\t\t\t"       # HEADER_ACAPIT
@@ -31,39 +31,37 @@ def main():
     print(f"{HA}\t{APP_NAME}")
 
     ACTIONS = ['login', 'register', 'exit']
-    print_actions(ACTIONS, "-")
-    print("")
-    selected = get_selected_option(input(":"), 1, len(ACTIONS))
-    if selected == 1:
-        username = start_login()
+    selected = 1
+    while 0 < selected < len(ACTIONS):
+        print("\n")
+        print_actions(ACTIONS, "-")
         print("")
-        print_passwords(get_passwords(username))
+        selected = get_selected_option(input(":"), 1, len(ACTIONS))
+        if selected == 1:
+            username = start_login()
+            start_getting_passwords(username)
 
-        LOGIN_ACTIONS = ['show passwords',
-                         'add password', 'decrypt password', 'decrypt all passwords',  'exit']
-        while 0 < selected < len(LOGIN_ACTIONS):
-            print("\n")
-            print_actions(LOGIN_ACTIONS, "-")
-            print("")
-            selected = get_selected_option(input(":"), 1, len(LOGIN_ACTIONS))
+            LOGIN_ACTIONS = ['show passwords',
+                             'add password', 'decrypt password', 'decrypt all passwords',  'exit']
+            while 0 < selected < len(LOGIN_ACTIONS):
+                print("\n")
+                print_actions(LOGIN_ACTIONS, "-")
+                print("")
+                selected = get_selected_option(
+                    input(":"), 1, len(LOGIN_ACTIONS))
 
-            if selected == 1:
-                print_passwords(get_passwords(username))
-            elif selected == 2:
-                start_adding_password(username)
-            elif selected == 3:
-                start_decrypting_password(username)
-            elif selected == 4:
-                passwords = get_passwords(username)
-                start_decrypting_all_passwords(username, passwords)
-            else:
-                print(f"{SA}See you next time")
-                sys.exit(0)
-    elif selected == 2:
-        start_register()
-    else:
-        print(f"{SA}See you next time")
-        sys.exit(0)
+                if selected == 1:
+                    start_getting_passwords(username)
+                elif selected == 2:
+                    start_adding_password(username)
+                elif selected == 3:
+                    start_decrypting_password(username)
+                elif selected == 4:
+                    start_decrypting_all_passwords(username)
+        elif selected == 2:
+            start_register()
+    print(f"{SA}See you next time")
+    return
 
 
 def print_actions(actions, prfx):
@@ -93,7 +91,6 @@ def print_passwords(passwords, decoded=False):
         print(f"{HA}There is no passwords yet.")
 
 
-# REGISTER
 def start_register():
     print(f"{HA}Sign Up")
     print('Enter your credentials\n')
@@ -102,51 +99,29 @@ def start_register():
     password1 = getpass("Repeat Password: ")
     master = getpass("Master Password: ")
     master1 = getpass("Repeat Master Password: ")
-    if not register(name, password, password1, master, master1):
-        print("Error! Something went wrong during registration")
-        sys.exit(0)
-    print("Welcome on board, now you can login")
+    if not um.register(name, password, password1, master, master1):
+        print(f"{TA}Error! Something went wrong during registration\n")
+        return
+    print(f"{TA}Welcome on board, now you can login\n")
 
 
-def register(name, password, password1, master, master1):
-    user = {'username': name, 'password': password,
-            'password1': password1, 'master': master, 'master1': master1}
-    if not v.is_user_valid(user):
-        return False
-    hashed, mhashed = hash_pass(user['password']), hash_pass(user['master'])
-    if db.register_user(user['username'], hashed, mhashed):
-        return True
-    return False
-
-
-def hash_pass(password, salt_length=SALT_LENGTH):
-    password, salt = password.encode(),  gensalt(salt_length)
-    return hashpw(password, salt)
-
-
-# LOGIN
 def start_login():
     print(f"{HA}Sign in")
     username, password = input("Username: "), getpass("Password: ")
-    if not login(username, password):
-        print("Error! Something went wrong during login")
-        sys.exit(0)
-    print("Welcome back " + username)
+    if not um.login(username, password):
+        print(f"{TA}Error! Something went wrong during login")
+        return
     return username
 
 
-def login(username, password):
-    if not v.is_username_login_valid(username) or not v.is_password_login_valid(password):
-        return False
-    if db.is_registred(username):
-        hashed = db.get_user_password(username)
-        if hashed:
-            if checkpw(password.encode(), hashed):
-                return True
-    return False
+def start_getting_passwords(username):
+    passwords = get_passwords(username)
+    if not passwords:
+        print(f"{TA}There was an error during getting passwords \n")
+    else:
+        print_passwords(passwords)
 
 
-# PASSWORDS
 def get_passwords(username):
     res = db.get_user_passwords(username)
     if res != None:
@@ -160,7 +135,6 @@ def get_passwords(username):
             p['password_id'] = r[4]
             passes.append(p)
         return passes
-    print("There was an error during getting password!")
     return None
 
 
@@ -214,37 +188,36 @@ def verify_master(username, master_password):
     return False
 
 
-def start_decrypting_all_passwords(username, passwords):
-    print(f"{HA}Decrypt All Passwords")
-    master_password = getpass("Master Password: ")
-    if not verify_master(username, master_password):
-        print("Error! Wrong Master Password")
-        return
-
-    for p in passwords:
-        password_id = p["password_id"]
-        encrypted = get_password(username, master_password, password_id)
-        p["service_password"] = encrypted
-    print_passwords(passwords, True)
-
-
 def start_decrypting_password(username):
     print(f"{HA}Decrypt Password")
     master_password = getpass("Master Password: ")
     if not verify_master(username, master_password):
         print("Error! Wrong Master Password")
         return
-
     password_id = input("Password ID: ")
     if len(password_id) != 6:
         print("There in no password with this id!")
         return
-
     decrypted_password = get_password(username, master_password, password_id)
     if not decrypted_password:
         print("There in no password with this id!")
         return
     print("Your password is " + decrypted_password)
+
+
+def start_decrypting_all_passwords(username):
+    print(f"{HA}Decrypt All Passwords")
+    master_password = getpass("Master Password: ")
+    if not verify_master(username, master_password):
+        print("Error! Wrong Master Password")
+        return
+
+    passwords = get_passwords(username)
+    for p in passwords:
+        password_id = p["password_id"]
+        encrypted = get_password(username, master_password, password_id)
+        p["service_password"] = encrypted
+    print_passwords(passwords, True)
 
 
 def get_password(username, master_password, password_id):
